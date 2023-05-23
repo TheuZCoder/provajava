e413c4bf927344b207492893dae375a60b4699bb
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-05-23 17:32:32 +0000

    23/05

--- Prova2305/Exercicios.java
+++ Prova2305/Exercicios.java
@@ -11,12 +11,9 @@ public class Exercicios {
     int linhas, colunas;
     int[][] matriz;
     Random aleatorio = new Random();
-        Scanner leitura = new Scanner(System.in);
+    Scanner leitura = new Scanner(System.in);
 
     public void ex1() {
-        
-        
-        
 
         System.out.println("digite o numero de linhas de sua matriz");
         linhas = leitura.nextInt();
@@ -56,6 +53,30 @@ public class Exercicios {
                 System.out.print(matriz[i][j] + "  ");
             }
             System.out.println("|");
+        }
+    }
+    public void ex2() {
+        int numeroSorteado = aleatorio.nextInt(1001);
+        int palpite;
+        int tentativas = 0;
+
+        System.out.println("digite um palpite entre 0 a 1000");
+        palpite = leitura.nextInt();
+        tentativas++;
+        if (palpite == numeroSorteado) {
+            System.out.println("PARABENS VOCÊ É INCRIVEL!!!!!!! ACERTOU");
+        } else {
+            while (palpite != numeroSorteado) {
+                if (palpite < numeroSorteado) {
+                    System.out.println("o palpite é menor que o numero sorteado");
+                } else {
+                    System.out.println("o palpite é maior que o numero sorteado");
+                }
+                System.out.println("Digite um novo palpite: ");
+                palpite = leitura.nextInt();
+                tentativas++;
+            }
+            System.out.println("Parabens!! você acertou o numero em "+ tentativas+ " tentativas");
         }
     }
 }
